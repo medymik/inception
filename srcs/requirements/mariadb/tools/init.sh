135c91aeb897426a80b1804f8f5eb4d932aff71a
mohammed ymik <mohammed.ymik@gmail.com>
2021-10-10 23:10:20 +0100

    fix mimes types and add mariadb to the network

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -6,7 +6,9 @@ then
     # mysql --user=root wordpress < wordpress.sql
     service mariadb stop
 fi
+chown -R root:root /var/lib/mysql
 sed -i 's/skip-networking/# skip-networking/g' /etc/my.cnf.d/mariadb-server.cnf
+sed -i 's/#bind-address/bind-address/g' /etc/my.cnf.d/mariadb-server.cnf
 service mariadb start
 service mariadb stop
-/usr/bin/mariadbd --basedir=/usr --datadir=/var/lib/mysql --plugin-dir=/usr/lib/mariadb/plugin --user=mysql --pid-file=/run/mysqld/mariadb.pid
+/usr/bin/mariadbd --basedir=/usr --datadir=/var/lib/mysql --plugin-dir=/usr/lib/mariadb/plugin --user=root --pid-file=/run/mysqld/mariadb.pid
